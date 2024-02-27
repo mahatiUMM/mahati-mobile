d1545eacb1361149f887ce0aaa24481419fa8056
Ahmad Naufal <naufalopal002@gmail.com>
2024-02-27 13:31:53 +0700

    style: style of application color, text, and text style

---
+++ lib/app/utils/Colors/color_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ColorApp {
+  //Widget Color
+  static Color primaryColor = const Color(0xFF40D99E);
+  static Color primaryColorOnGradient = const Color(0x0040D99E);
+  static Color backgroundColor = const Color(0xFFFFFFFF);
+  static Color textFieldColor = const Color(0xFFF5F6F8);
+  //Text Color
+  static Color titleColor = const Color(0xFF11371C);
+  static Color subTitleColor = const Color(0xFF383838);
+  static Color hintColor = const Color(0xFFA0A0A0);
+}
---
+++ lib/app/utils/constants/text_style.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:mahati_mobile/app/utils/Colors/color_app.dart';
+import 'package:sizer/sizer.dart';
+
+class StyleText {
+  //Auth text style
+  static TextStyle authTitle1 = TextStyle(
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700,
+      height: 0,
+      color: ColorApp.titleColor,
+      decoration: TextDecoration.none,
+      fontSize: 23.sp);
+
+  static TextStyle authTitle2 = TextStyle(
+    decoration: TextDecoration.none,
+    color: ColorApp.subTitleColor,
+    fontSize: 12.sp,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle signInField = TextStyle(
+      color: ColorApp.hintColor,
+      fontSize: 10.sp,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w500);
+
+  static TextStyle authTextButton = TextStyle(
+    decoration: TextDecoration.none,
+    color: ColorApp.primaryColor,
+    fontSize: 12.sp,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle authElevatedButton = TextStyle(
+    decoration: TextDecoration.none,
+    color: ColorApp.backgroundColor,
+    fontSize: 12.sp,
+    fontFamily: 'Inter',
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle signInSubtitle1 = TextStyle(
+      decoration: TextDecoration.none,
+      color: ColorApp.subTitleColor,
+      fontSize: 12.sp,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w400);
+
+  static TextStyle signInSubtitle2 = TextStyle(
+      decoration: TextDecoration.none,
+      color: ColorApp.subTitleColor,
+      fontSize: 12.sp,
+      fontFamily: 'Inter',
+      fontWeight: FontWeight.w700);
+}
