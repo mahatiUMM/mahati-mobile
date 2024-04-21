76da01739a467d89cf65e5d23900bdeb7e12d671
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-04-21 22:19:50 +0700

    feat (inbox): add inbox message

--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -3,6 +3,7 @@ import 'package:get/get.dart';
 import 'package:mahati_mobile/app/modules/home/home_controller.dart';
 import 'package:mahati_mobile/app/modules/home/widget/greeting.dart';
 import 'package:mahati_mobile/app/modules/home/widget/slider.dart';
+import 'package:mahati_mobile/app/modules/home/widget/tab_box.dart';
 import 'package:mahati_mobile/app/utils/resources.dart';
 import 'package:sizer/sizer.dart';
 
@@ -28,6 +29,8 @@ class HomeView extends GetView<HomeController> {
                 ),
                 const SizedBox(height: 20),
                 sliderWidget(),
+                const SizedBox(height: 20),
+                TabBox(),
               ],
             ),
           ),
--- lib/app/modules/home/widget/slider.dart
+++ lib/app/modules/home/widget/slider.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:mahati_mobile/app/utils/resources.dart';
 
 Widget sliderWidget() {
   return Container(
     width: 350,
     height: 200,
     decoration: ShapeDecoration(
-      color: const Color(0xFF808080),
+      color: Resources.color.baseColor2,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(20),
       ),
