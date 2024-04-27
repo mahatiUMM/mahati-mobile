996f5dd327a057b7e848ca113c2c6313d802e4f8
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-04-28 06:52:19 +0700

    merge (home & education): remove icon button

--- lib/app/modules/home/home_view.dart
+++ lib/app/modules/home/home_view.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_floating_bottom_bar/flutter_floating_bottom_bar.dart';
 import 'package:get/get.dart';
 import 'package:mahati_mobile/app/modules/home/home_controller.dart';
 import 'package:mahati_mobile/app/modules/home/widget/greeting.dart';
 import 'package:mahati_mobile/app/modules/home/widget/slider.dart';
 import 'package:mahati_mobile/app/modules/home/widget/tab_box.dart';
-import 'package:mahati_mobile/app/utils/Colors/color_app.dart';
 import 'package:mahati_mobile/app/utils/resources.dart';
 import 'package:sizer/sizer.dart';
 
@@ -19,72 +17,30 @@ class HomeView extends GetView<HomeController> {
         return Scaffold(
           resizeToAvoidBottomInset: false,
           backgroundColor: AppColors.backgroundHome,
-          body: BottomBar(
-              width: 70.w,
-              barColor: Colors.white,
-              borderRadius: BorderRadius.circular(30),
-              body: (context, controller) => SingleChildScrollView(
-                    child: SizedBox(
-                      width: Get.width,
-                      height: Get.height,
-                      child: const Column(
-                        children: [
-                          Row(
-                            children: [
-                              GreetingWidget(
-                                /*
+          body: SingleChildScrollView(
+            child: SizedBox(
+              width: Get.width,
+              height: Get.height,
+              child: const Column(
+                children: [
+                  Row(
+                    children: [
+                      GreetingWidget(
+                        /*
                           ! Change the username value from API MAHATI
                           */
-                                username: 'Rizky Haksono',
-                              ),
-                            ],
-                          ),
-                          SizedBox(height: 20),
-                          SliderWidget(),
-                          SizedBox(height: 20),
-                          TabBox(),
-                        ],
+                        username: 'Rizky Haksono',
                       ),
-                    ),
+                    ],
                   ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      print("Item 1");
-                    },
-                    icon: Icon(
-                      Icons.home_outlined,
-                      color: ColorApp.primaryColor1,
-                      size: 24,
-                    ),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      print("item 2");
-                    },
-                    icon: Icon(
-                      Icons.inbox_outlined,
-                      color: ColorApp.baseColor,
-                      size: 24,
-                    ),
-                  ),
-                  IconButton(
-                    padding: EdgeInsets.zero,
-                    onPressed: () {
-                      print("item 3");
-                    },
-                    icon: Icon(
-                      Icons.person_outline,
-                      color: ColorApp.baseColor,
-                      size: 24,
-                    ),
-                  ),
+                  SizedBox(height: 20),
+                  SliderWidget(),
+                  SizedBox(height: 20),
+                  TabBox(),
                 ],
-              )),
+              ),
+            ),
+          ),
         );
       },
     );
