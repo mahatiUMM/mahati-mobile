6fbef90a845c3dbe2390968bee4540ef6182ccd8
xyzuan <xyzuannihboss@gmail.com>
2024-05-02 01:06:45 +0700

    fix(auth): Handle autoLogin when JWT token are expired

--- lib/app/modules/splash/splash_controller.dart
+++ lib/app/modules/splash/splash_controller.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+import 'dart:math';
+
 import 'package:get/get.dart';
+import 'package:mahati_mobile/app/core/data/profile_model.dart';
+import 'package:mahati_mobile/app/core/network/rest_client.dart';
 import 'package:mahati_mobile/app/modules/onboard/onboard_view.dart';
+import 'package:mahati_mobile/app/utils/show_bar/show_bar.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashController extends GetxController {
@@ -18,10 +24,19 @@ class SplashController extends GetxController {
 
   _navigateToOnboardScreen() async {
     final token = await getToken();
+    final restClient = Get.find<RestClient>();
+    final result = await restClient.requestWithToken(
+        "/profile", HttpMethod.GET, null, token.toString());
+    Map<String, dynamic> resultData = json.decode(result.body);
 
     await Future.delayed(const Duration(seconds: 3));
     if (token != null) {
-      Get.offAndToNamed('/layout');
+      if (resultData['status'] == 401) {
+        showErrorMessage('JWT Token Expired. Please login again');
+        Get.off(() => const OnboardView());
+      } else {
+        Get.offAndToNamed('/layout');
+      }
     } else {
       Get.off(() => const OnboardView());
     }
