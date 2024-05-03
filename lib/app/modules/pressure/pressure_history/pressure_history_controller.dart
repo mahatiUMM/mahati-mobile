563ec33514a97c6bad99e38464377e754815d061
xyzuan <xyzuannihboss@gmail.com>
2024-05-03 16:06:02 +0700

    feat(pressure/history): Add Pressure chart

--- lib/app/modules/pressure/pressure_history/pressure_history_controller.dart
+++ lib/app/modules/pressure/pressure_history/pressure_history_controller.dart
@@ -2,18 +2,29 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:mahati_mobile/app/core/data/blood_pressure_model.dart';
 import 'package:mahati_mobile/app/core/network/rest_client.dart';
+
+class PressureData {
+  final int sistol;
+  final int diastole;
+  final int heartbeat;
+  final DateTime datetime;
+
+  PressureData(this.sistol, this.diastole, this.heartbeat, this.datetime);
+}
 
 class PressureHistoryController extends GetxController
     with SingleGetTickerProviderMixin {
   final List<Tab> myTabs = <Tab>[
-    Tab(text: 'Tekanan Darah'),
-    Tab(text: 'Detak Jantung'),
+    const Tab(text: 'Tekanan Darah'),
+    const Tab(text: 'Detak Jantung'),
   ];
 
   late TabController tabController;
   var pressureHistory = Rx<List<dynamic>>([]);
+  // var currentSistoleHistory = Rx(String);
+  // var currentDiastoleHistory = Rx(String);
+  // var currentHeartbeatHistory = Rx(String);
 
   @override
   void onInit() {
@@ -30,17 +41,12 @@ class PressureHistoryController extends GetxController
 
   Future<void> getUserPressureHistory() async {
     final restClient = Get.find<RestClient>();
-
     final result =
         await restClient.request("/blood_pressure", HttpMethod.GET, null);
 
-    // Parse the JSON string
     Map<String, dynamic> parsedJson = json.decode(result.body);
-
-    // Access the data key
     List<dynamic> dataList = parsedJson['data'];
+    dataList.sort((a, b) => b['created_at'].compareTo(a['created_at']));
     pressureHistory.value = dataList;
-    // var responseData = BloodPressureModel.fromJson(jsonDecode(result.body));
-    // print(responseData);
   }
 }
--- lib/app/modules/pressure/pressure_history/pressure_history_view.dart
+++ lib/app/modules/pressure/pressure_history/pressure_history_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:mahati_mobile/app/modules/pressure/pressure_history/pressure_history_controller.dart';
 import 'package:mahati_mobile/app/utils/resources.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
 class PressureHistoryView extends GetView<PressureHistoryController> {
   const PressureHistoryView({Key? key}) : super(key: key);
@@ -54,13 +55,210 @@ class PressureHistoryView extends GetView<PressureHistoryController> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Obx(
-                  // () => Text(
-                  //   controller.pressureHistory.value.toString(),
-                  // ),
                   () => Column(
-                    children: controller.pressureHistory.value
-                        .map((e) => Text(e.toString()))
-                        .toList(),
+                    children: [
+                      Container(
+                          width: Get.width,
+                          decoration: ShapeDecoration(
+                            color: Colors.white,
+                            shadows: [
+                              BoxShadow(
+                                color: Colors.grey
+                                    .withOpacity(0.15), // Shadow color
+                                spreadRadius: 8, // Spread radius
+                                blurRadius: 12, // Blur radius
+                                offset: const Offset(0, 3), // Offset
+                              ),
+                            ],
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15),
+                            ),
+                          ),
+                          child: Padding(
+                            padding: const EdgeInsets.all(18),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                const Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Column(
+                                      children: [
+                                        Text(
+                                          "Systolic",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w500,
+                                            height: 0,
+                                          ),
+                                        ),
+                                        Text("(mmHg)")
+                                      ],
+                                    ),
+                                    Text(
+                                      '50',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Color(0xFFE9CC66),
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.w700,
+                                        height: 0.03,
+                                        letterSpacing: 0.06,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Column(
+                                      children: [
+                                        Text(
+                                          "Diastolic",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w500,
+                                            height: 0,
+                                          ),
+                                        ),
+                                        const Text("(mmHg)")
+                                      ],
+                                    ),
+                                    Text(
+                                      '50',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Resources.color.tertiaryColor,
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.w700,
+                                        height: 0.03,
+                                        letterSpacing: 0.06,
+                                      ),
+                                    )
+                                  ],
+                                ),
+                                const SizedBox(height: 10),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Column(
+                                      children: [
+                                        Text(
+                                          "Pulse",
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                            fontSize: 15,
+                                            fontFamily: 'Poppins',
+                                            fontWeight: FontWeight.w500,
+                                            height: 0,
+                                          ),
+                                        ),
+                                        Text(
+                                          "(BMP)",
+                                        )
+                                      ],
+                                    ),
+                                    Text(
+                                      '50',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                        color: Resources.color.primaryColor,
+                                        fontSize: 32,
+                                        fontWeight: FontWeight.w700,
+                                        height: 0.03,
+                                        letterSpacing: 0.06,
+                                      ),
+                                    )
+                                  ],
+                                )
+                              ],
+                            ),
+                          )),
+                      const SizedBox(height: 30),
+                      Container(
+                        decoration: ShapeDecoration(
+                          color: Colors.white,
+                          shadows: [
+                            BoxShadow(
+                              color:
+                                  Colors.grey.withOpacity(0.15), // Shadow color
+                              spreadRadius: 8, // Spread radius
+                              blurRadius: 12, // Blur radius
+                              offset: const Offset(0, 3), // Offset
+                            ),
+                          ],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15),
+                          ),
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.fromLTRB(12, 24, 12, 12),
+                          child: SfCartesianChart(
+                              legend: const Legend(
+                                  isVisible: true,
+                                  position: LegendPosition.bottom),
+                              primaryXAxis: const CategoryAxis(),
+                              primaryYAxis: const NumericAxis(
+                                axisLine: AxisLine(width: 0),
+                              ),
+                              series: <CartesianSeries>[
+                                ColumnSeries<PressureData, String>(
+                                  color: const Color(0xFFE9CC66),
+                                  dataSource: controller.pressureHistory.value
+                                      .map((e) => PressureData(
+                                          e['sistol'],
+                                          e['diastole'],
+                                          e['heartbeat'],
+                                          DateTime.parse(e['created_at'])))
+                                      .toList(),
+                                  xValueMapper: (PressureData pressure, _) =>
+                                      '${pressure.datetime.day}/${pressure.datetime.month}/${pressure.datetime.year}',
+                                  yValueMapper: (PressureData pressure, _) =>
+                                      pressure.sistol,
+                                  name: 'Systolic',
+                                ),
+                                ColumnSeries<PressureData, String>(
+                                  color: Resources.color.tertiaryColor1,
+                                  dataSource: controller.pressureHistory.value
+                                      .map((e) => PressureData(
+                                          e['sistol'],
+                                          e['diastole'],
+                                          e['heartbeat'],
+                                          DateTime.parse(e['created_at'])))
+                                      .toList(),
+                                  xValueMapper: (PressureData pressure, _) =>
+                                      '${pressure.datetime.day}/${pressure.datetime.month}/${pressure.datetime.year}',
+                                  yValueMapper: (PressureData pressure, _) =>
+                                      pressure.diastole,
+                                  name: 'Diastole',
+                                ),
+                                ColumnSeries<PressureData, String>(
+                                  color: Resources.color.primaryColor,
+                                  dataSource: controller.pressureHistory.value
+                                      .map((e) => PressureData(
+                                          e['sistol'],
+                                          e['diastole'],
+                                          e['heartbeat'],
+                                          DateTime.parse(e['created_at'])))
+                                      .toList(),
+                                  xValueMapper: (PressureData pressure, _) =>
+                                      '${pressure.datetime.day}/${pressure.datetime.month}/${pressure.datetime.year}',
+                                  yValueMapper: (PressureData pressure, _) =>
+                                      pressure.heartbeat,
+                                  name: 'Heartbeat',
+                                ),
+                              ]),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
