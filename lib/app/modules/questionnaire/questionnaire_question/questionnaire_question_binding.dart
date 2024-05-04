5ab34a70362746eb3b790bb0933869b917ee7c7d
arizkyiqbal <arizkyiqbal@gmail.com>
2024-05-04 23:22:36 +0700

    feat: add questionnairequestion view, controller, binding and widget

---
+++ lib/app/modules/questionnaire/questionnaire_question/questionnaire_question_binding.dart
@@ -0,0 +1,9 @@
+import 'package:get/get.dart';
+import 'package:mahati_mobile/app/modules/questionnaire/questionnaire_question/questionnaire_question_controller.dart';
+
+class QuestionnaireQuestionBinding extends Bindings {
+  @override
+  void dependencies() {
+    Get.lazyPut<QuestionnaireQuestionController>(() => QuestionnaireQuestionController());
+  }
+}
