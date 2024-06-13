f441dfac0e51fba95a9d2d638f71f1954c5f1012
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-06-13 21:30:28 +0700

    imp(reminder): refactor refill form

--- lib/app/modules/reminder/reminder_refiil/widget/reminder_refill_form.dart
+++ lib/app/modules/reminder/reminder_refiil/widget/reminder_refill_form.dart
@@ -13,238 +13,217 @@ Widget reminderRefillForm(BuildContext context) {
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 15),
-        Text(
-          "Nama Obat",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        TextField(
+        buildTextField(
+          label: "Nama Obat",
+          hintText: "Masukkan nama obat",
           controller: controller.namaObatController,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Resources.color.textFieldColor,
-            hintText: "Masukkan nama obat",
-            hintStyle: TextStyle(
-              color: Resources.color.hintColor,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
         ),
         const SizedBox(height: 15),
-        Text(
-          "Obat yang Sudah Diminum",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        TextField(
+        buildTextField(
+          label: "Obat yang Sudah Diminum",
+          hintText: "Masukkan obat yang sudah diminum",
           controller: controller.takenObatController,
           keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Resources.color.textFieldColor,
-            hintText: "Masukkan obat yang sudah diminum",
-            hintStyle: TextStyle(
-              color: Resources.color.hintColor,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
         ),
         const SizedBox(height: 15),
-        Text(
-          "Total Obat",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        TextField(
+        buildTextField(
+          label: "Total Obat",
+          hintText: "Masukkan total obat",
           controller: controller.amountObatController,
           keyboardType: TextInputType.number,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Resources.color.textFieldColor,
-            hintText: "Masukkan total obat",
-            hintStyle: TextStyle(
-              color: Resources.color.hintColor,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
         ),
         const SizedBox(height: 15),
-        Text(
-          "Penyebab Sakit",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        TextField(
+        buildTextField(
+          label: "Penyebab Sakit",
+          hintText: "Masukkan penyebab sakit",
           controller: controller.causeObatController,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Resources.color.textFieldColor,
-            hintText: "Masukkan penyebab sakit",
-            hintStyle: TextStyle(
-              color: Resources.color.hintColor,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
         ),
         const SizedBox(height: 15),
-        Text(
-          "Ukuran Kapsul",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
-          ),
-        ),
-        DropdownButtonFormField<String>(
-          value: controller.selectedCapsuleSize,
-          items: <String>['Tidak Kuat', 'Sedang', 'Kuat'].map((String value) {
-            return DropdownMenuItem<String>(
-              onTap: () {
-                controller.capSizeObatController.text = value;
-              },
-              value: value,
-              child: Text(value),
-            );
-          }).toList(),
+        buildDropdown(
+          label: "Ukuran Kapsul",
+          items: ['Tidak Kuat', 'Sedang', 'Kuat'],
+          selectedValue: controller.selectedCapsuleSize,
           onChanged: (newValue) {
-            controller.selectedCapsuleSize = newValue;
+            controller.selectedCapsuleSize = newValue!;
+            controller.capSizeObatController.text = newValue;
           },
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Resources.color.textFieldColor,
-            hintText: "Pilih ukuran kapsul",
-            hintStyle: TextStyle(
-              color: Resources.color.hintColor,
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Resources.color.textFieldColor),
-              borderRadius: BorderRadius.circular(15),
-            ),
-          ),
         ),
         const SizedBox(height: 15),
-        Text(
-          "Waktu Pengingat Obat",
-          style: TextStyle(
-            decoration: TextDecoration.none,
-            color: Resources.color.subtitleColor,
-            fontSize: 11.sp,
-            fontFamily: Resources.font.primaryFont,
-            fontWeight: FontWeight.w500,
+        buildTimePickerField(
+          context,
+          label: "Waktu Pengingat Obat",
+          controller: controller.timeObatController,
+          onTap: () => controller.selectTime(context),
+        ),
+        const SizedBox(height: 20),
+        buildSaveButton(controller),
+      ],
+    ),
+  );
+}
+
+Widget buildTextField({
+  required String label,
+  required String hintText,
+  required TextEditingController controller,
+  TextInputType keyboardType = TextInputType.text,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          color: Resources.color.subtitleColor,
+          fontSize: 11.sp,
+          fontFamily: Resources.font.primaryFont,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      TextField(
+        controller: controller,
+        keyboardType: keyboardType,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Resources.color.textFieldColor,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Resources.color.hintColor),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Resources.color.textFieldColor),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Resources.color.textFieldColor),
+            borderRadius: BorderRadius.circular(15),
           ),
         ),
-        GestureDetector(
-          onTap: () {
-            controller.selectTime(context);
-          },
-          child: AbsorbPointer(
-            child: TextField(
-              controller: controller.timeObatController,
-              decoration: InputDecoration(
-                filled: true,
-                fillColor: Resources.color.textFieldColor,
-                hintText: controller.timeObatController.text.isEmpty
-                    ? "Pilih waktu pengingat obat (per hari)"
-                    : controller.timeObatController.text,
-                hintStyle: TextStyle(
-                  color: Resources.color.hintColor,
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Resources.color.textFieldColor),
-                  borderRadius: BorderRadius.circular(15),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderSide: BorderSide(color: Resources.color.textFieldColor),
-                  borderRadius: BorderRadius.circular(15),
-                ),
+      ),
+    ],
+  );
+}
+
+Widget buildDropdown({
+  required String label,
+  required List<String> items,
+  required String? selectedValue,
+  required void Function(String?) onChanged,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          color: Resources.color.subtitleColor,
+          fontSize: 11.sp,
+          fontFamily: Resources.font.primaryFont,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      DropdownButtonFormField<String>(
+        value: selectedValue,
+        items: items.map((String value) {
+          return DropdownMenuItem<String>(
+            value: value,
+            child: Text(value),
+          );
+        }).toList(),
+        onChanged: onChanged,
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Resources.color.textFieldColor,
+          hintText: "Pilih ukuran kapsul",
+          hintStyle: TextStyle(color: Resources.color.hintColor),
+          enabledBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Resources.color.textFieldColor),
+            borderRadius: BorderRadius.circular(15),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Resources.color.textFieldColor),
+            borderRadius: BorderRadius.circular(15),
+          ),
+        ),
+      ),
+    ],
+  );
+}
+
+Widget buildTimePickerField(
+  BuildContext context, {
+  required String label,
+  required TextEditingController controller,
+  required void Function() onTap,
+}) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        label,
+        style: TextStyle(
+          decoration: TextDecoration.none,
+          color: Resources.color.subtitleColor,
+          fontSize: 11.sp,
+          fontFamily: Resources.font.primaryFont,
+          fontWeight: FontWeight.w500,
+        ),
+      ),
+      GestureDetector(
+        onTap: onTap,
+        child: AbsorbPointer(
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Resources.color.textFieldColor,
+              hintText: controller.text.isEmpty
+                  ? "Pilih waktu pengingat obat (per hari)"
+                  : controller.text,
+              hintStyle: TextStyle(color: Resources.color.hintColor),
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Resources.color.textFieldColor),
+                borderRadius: BorderRadius.circular(15),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Resources.color.textFieldColor),
+                borderRadius: BorderRadius.circular(15),
               ),
             ),
           ),
         ),
-        const SizedBox(height: 20),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: Resources.color.primaryColor,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10),
-            ),
-            minimumSize: const Size(double.infinity, 50),
-          ),
-          onPressed: () {
-            controller.postReminder(
-              controller.namaObatController.text,
-              int.parse(controller.takenObatController.text),
-              int.parse(controller.amountObatController.text),
-              controller.causeObatController.text,
-              controller.capSizeObatController.text,
-              controller.timeObatController.text,
-            );
-          },
-          child: Text(
-            "Simpan",
-            style: TextStyle(
-              color: Resources.color.whiteColor,
-              fontSize: 11.sp,
-              fontFamily: Resources.font.primaryFont,
-              fontWeight: FontWeight.w700,
-            ),
-          ),
-        ),
-      ],
+      ),
+    ],
+  );
+}
+
+Widget buildSaveButton(ReminderRefillController controller) {
+  return ElevatedButton(
+    style: ElevatedButton.styleFrom(
+      backgroundColor: Resources.color.primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      minimumSize: const Size(double.infinity, 50),
+    ),
+    onPressed: () {
+      controller.postReminder(
+        controller.namaObatController.text,
+        int.parse(controller.takenObatController.text),
+        int.parse(controller.amountObatController.text),
+        controller.causeObatController.text,
+        controller.capSizeObatController.text,
+        controller.timeObatController.text,
+      );
+    },
+    child: Text(
+      "Simpan",
+      style: TextStyle(
+        color: Resources.color.whiteColor,
+        fontSize: 11.sp,
+        fontFamily: Resources.font.primaryFont,
+        fontWeight: FontWeight.w700,
+      ),
     ),
   );
 }
