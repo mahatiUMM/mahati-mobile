1c16548891f9bea634d112b3a2f4c160b4a76f5d
rizkyhaksono <mrizkyhaksono@gmail.com>
2024-09-17 22:44:07 +0700

    test(splash): add expect on title and description

---
+++ integration_test/screens/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mahati_mobile/app/core/network/rest_client.dart';
+import 'package:mahati_mobile/app/modules/splash/splash_controller.dart';
+import 'package:mahati_mobile/app/modules/splash/splash_view.dart';
+
+void main() {
+  testWidgets("Test title on Splash Screen UI", (WidgetTester tester) async {
+    Get.put(SplashController());
+    Get.put(RestClient());
+    await tester.pumpWidget(const MaterialApp(home: SplashView()));
+
+    // expect(find.byType(SplashView), findsOneWidget);
+    expect(find.text("mahati."), findsOneWidget);
+    expect(find.textContaining("mahati."), findsOneWidget);
+  });
+
+  // testWidgets("Test description on Splash Screen UI",
+  //     (WidgetTester tester) async {
+  //   Get.put(SplashController());
+  //   Get.put(RestClient());
+  //   await tester.pumpWidget(const MaterialApp(home: SplashView()));
+
+  //   expect(find.text("Mobile Aplikasi Sahabat\nHipertensi"), findsOneWidget);
+  // });
+}
