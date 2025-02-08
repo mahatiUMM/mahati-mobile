f4fbb16f757f3ae6a43ed85999ec1123af8ff122
Ahmad Naufal <naufalopal002@gmail.com>
2025-02-08 22:49:55 +0700

    feat: refresh indicator di questionnaire

--- lib/app/modules/questionnaire/widget/questionnaire_history.dart
+++ lib/app/modules/questionnaire/widget/questionnaire_history.dart
@@ -1,3 +1,4 @@
+import 'package:custom_refresh_indicator/custom_refresh_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:get/get.dart';
@@ -16,102 +17,116 @@ class QuestionnaireHistory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() => controller.isLoadingHistoryQuestionnaire.isTrue
-        ? Center(
-            child: CircularProgressIndicator(
-                valueColor: AlwaysStoppedAnimation<Color>(
-                    Resources.color.primaryColor)),
-          )
-        : SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                top: 20,
-                left: 20,
-                right: 20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Obx(
-                    () => Text(
-                      "Hai, ${controller.username.value}",
-                      style: TextStyle(
-                          fontFamily: Resources.font.primaryFont,
-                          fontSize: 18,
-                          fontWeight: FontWeight.w700),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 4.0),
-                    child: Text(
-                      textAlign: TextAlign.justify,
-                      "Anda dapat melihat kuesioner yang sudah dilakukan dibawah ini",
-                      style: TextStyle(
-                        fontFamily: Resources.font.primaryFont,
-                        color: Colors.grey,
+    return CustomMaterialIndicator(
+      onRefresh: () => controller.refreshQuestionnaire(),
+      backgroundColor: Resources.color.backgroundHome2,
+      indicatorBuilder: (context, controller) {
+        return Padding(
+          padding: const EdgeInsets.all(6.0),
+          child: CircularProgressIndicator(
+            color: Resources.color.primaryColor,
+            value: controller.state.isLoading ? null : null,
+          ),
+        );
+      },
+      child: Obx(() => controller.isLoadingHistoryQuestionnaire.isTrue
+          ? Center(
+              child: CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                      Resources.color.primaryColor)),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(
+                  top: 20,
+                  left: 20,
+                  right: 20,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Obx(
+                      () => Text(
+                        "Hai, ${controller.username.value}",
+                        style: TextStyle(
+                            fontFamily: Resources.font.primaryFont,
+                            fontSize: 18,
+                            fontWeight: FontWeight.w700),
                       ),
                     ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const Padding(
-                        padding: EdgeInsets.only(top: 18.0),
-                        child: Text(
-                          "Semua Kuesioner",
-                          style: TextStyle(
-                            fontSize: 14,
-                            fontWeight: FontWeight.w500,
-                          ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4.0),
+                      child: Text(
+                        textAlign: TextAlign.justify,
+                        "Anda dapat melihat kuesioner yang sudah dilakukan dibawah ini",
+                        style: TextStyle(
+                          fontFamily: Resources.font.primaryFont,
+                          color: Colors.grey,
                         ),
                       ),
-                      Obx(() {
-                        if (controller.isLoadingAvailableQuestionnaire.value) {
-                          return Center(
-                            child: CircularProgressIndicator(
-                              valueColor: AlwaysStoppedAnimation<Color>(
-                                  Theme.of(context).colorScheme.secondary),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(top: 18.0),
+                          child: Text(
+                            "Semua Kuesioner",
+                            style: TextStyle(
+                              fontSize: 14,
+                              fontWeight: FontWeight.w500,
                             ),
-                          );
-                        } else {
-                          return ListView.builder(
-                            shrinkWrap: true,
-                            physics: const NeverScrollableScrollPhysics(),
-                            itemCount:
-                                controller.questionnaireUserAlreadyUse.length,
-                            itemBuilder: (context, index) {
-                              final item =
-                                  controller.questionnaireUserAlreadyUse[index];
-                              return Padding(
-                                padding: const EdgeInsets.only(top: 18),
-                                child: QuestionnaireCard(
-                                  isFilled: false,
-                                  title: item.title,
-                                  description: item.description,
-                                  imageUrl: item.image?.isNotEmpty == true
-                                      ? dotenv.get('IMAGE_URL') + item.image!
-                                      : 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png?20210521171500',
-                                  onPressed: () {
-                                    Get.toNamed('/questionnaire_question',
-                                        arguments: {
-                                          'id': item.id,
-                                          'image': item.image,
-                                          'title': item.title,
-                                          'desc': item.description,
-                                          'type': 'history'
-                                        });
-                                  },
-                                ),
-                              );
-                            },
-                          );
-                        }
-                      }),
-                    ],
-                  )
-                ],
+                          ),
+                        ),
+                        Obx(() {
+                          if (controller
+                              .isLoadingAvailableQuestionnaire.value) {
+                            return Center(
+                              child: CircularProgressIndicator(
+                                valueColor: AlwaysStoppedAnimation<Color>(
+                                    Theme.of(context).colorScheme.secondary),
+                              ),
+                            );
+                          } else {
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount:
+                                  controller.questionnaireUserAlreadyUse.length,
+                              itemBuilder: (context, index) {
+                                final item = controller
+                                    .questionnaireUserAlreadyUse[index];
+                                return Padding(
+                                  padding: const EdgeInsets.only(top: 18),
+                                  child: QuestionnaireCard(
+                                    isFilled: false,
+                                    title: item.title,
+                                    description: item.description,
+                                    imageUrl: item.image?.isNotEmpty == true
+                                        ? dotenv.get('IMAGE_URL') + item.image!
+                                        : 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png?20210521171500',
+                                    onPressed: () {
+                                      Get.toNamed('/questionnaire_question',
+                                          arguments: {
+                                            'id': item.id,
+                                            'image': item.image,
+                                            'title': item.title,
+                                            'desc': item.description,
+                                            'type': 'history'
+                                          });
+                                    },
+                                  ),
+                                );
+                              },
+                            );
+                          }
+                        }),
+                      ],
+                    )
+                  ],
+                ),
               ),
-            ),
-          ));
+            )),
+    );
   }
 }
